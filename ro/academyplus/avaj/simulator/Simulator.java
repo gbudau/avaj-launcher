7fb479f9365426b6ba1f705f349a4de4092521e0
gbudau <54190400+gbudau@users.noreply.github.com>
2025-10-24 14:12:14 +0200

    Initial commit

---
+++ ro/academyplus/avaj/simulator/Simulator.java
@@ -0,0 +1,28 @@
+package ro.academyplus.avaj.simulator;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Simulator {
+    void main(String[] args) {
+        if  (args.length != 1) {
+            System.err.println("Usage: java ro.academyplus.avaj.simulator.Simulator <scenario file>");
+            System.exit(1);
+        }
+
+        Path scenario = Path.of(args[0]);
+
+        try {
+            List<String> lines = Files.readAllLines(scenario.toAbsolutePath());
+            System.out.println("Loaded scenario file: " + scenario);
+            for (String line : lines) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Unable to read scenario: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+}
