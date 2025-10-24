0aed4ae54a1479a3bbea369f124239d0db538c75
gbudau <54190400+gbudau@users.noreply.github.com>
2025-10-24 16:34:40 +0200

    Add WeatherTower

---
+++ ro/academyplus/avaj/simulator/WeatherTower.java
@@ -0,0 +1,11 @@
+package ro.academyplus.avaj.simulator;
+
+class WeatherTower extends Tower {
+    public String getWeather(Coordinates p_coordinates) {
+        return null;
+    }
+
+    public void changeWeather() {
+
+    }
+}
