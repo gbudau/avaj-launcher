fa566bce43a654d6097298f304cad505e7865f9b
gbudau <54190400+gbudau@users.noreply.github.com>
2025-10-25 02:15:01 +0200

    Implement WeatherTower methods

--- ro/academyplus/avaj/tower/WeatherTower.java
+++ ro/academyplus/avaj/tower/WeatherTower.java
@@ -1,13 +1,14 @@
 package ro.academyplus.avaj.tower;
 
 import ro.academyplus.avaj.model.Coordinates;
+import ro.academyplus.avaj.provider.WeatherProvider;
 
 public final class WeatherTower extends Tower {
     public String getWeather(Coordinates p_coordinates) {
-        return null;
+        return WeatherProvider.getCurrentWeather(p_coordinates);
     }
 
     public void changeWeather() {
-
+        conditionChanged();
     }
 }
