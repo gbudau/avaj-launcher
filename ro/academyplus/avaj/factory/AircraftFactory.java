7a22317b625811e1ea6ff6641f6383826ecb8528
gbudau <54190400+gbudau@users.noreply.github.com>
2025-10-25 02:13:24 +0200

    Add AircraftFactory

--- ro/academyplus/avaj/factory/AircraftFactory.java
+++ ro/academyplus/avaj/factory/AircraftFactory.java
@@ -1,1 +1,49 @@
 package ro.academyplus.avaj.factory;
+
+import ro.academyplus.avaj.aircraft.Flyable;
+import ro.academyplus.avaj.aircraft.Balloon;
+import ro.academyplus.avaj.aircraft.Helicopter;
+import ro.academyplus.avaj.aircraft.Jetplane;
+import ro.academyplus.avaj.model.Coordinates;
+import java.util.concurrent.atomic.AtomicLong;
+
+final class AircraftFactory {
+    private static volatile AircraftFactory instance;
+    private static final AtomicLong id = new AtomicLong(0);
+
+    private AircraftFactory() {}
+
+    private static AircraftFactory getInstance() {
+        AircraftFactory aircraftFactory = instance;
+
+        if (aircraftFactory != null) {
+            return aircraftFactory;
+        }
+        synchronized (AircraftFactory.class) {
+            if (instance == null) {
+                instance = new AircraftFactory();
+            }
+            return instance;
+        }
+    }
+
+    private static long getNextId() {
+        return id.incrementAndGet();
+    }
+
+    private static Flyable createAircraft(String p_type, String p_name, Coordinates p_coordinates) {
+        return switch (p_type) {
+            case "Helicopter" -> new Helicopter(getNextId(), p_name, p_coordinates);
+            case "Jetplane" -> new Jetplane(getNextId(), p_name, p_coordinates);
+            case "Balloon" -> new Balloon(getNextId(), p_name, p_coordinates);
+            // TODO: Throw a custom exception
+            default -> throw new IllegalStateException("Unexpected value: " + p_type);
+        };
+    }
+
+    public static Flyable newAircraft(String p_type, String p_name, Coordinates p_coordinates) {
+        return AircraftFactory
+                .getInstance()
+                .createAircraft(p_type, p_name, p_coordinates);
+    }
+}
