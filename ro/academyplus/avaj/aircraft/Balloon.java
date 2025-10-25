f7aced484b0c85839f07fc13600e67a9d63cf61d
gbudau <54190400+gbudau@users.noreply.github.com>
2025-10-25 02:15:49 +0200

    Implement Balloon updateCondition method

--- ro/academyplus/avaj/aircraft/Balloon.java
+++ ro/academyplus/avaj/aircraft/Balloon.java
@@ -3,12 +3,44 @@ package ro.academyplus.avaj.aircraft;
 import ro.academyplus.avaj.model.Coordinates;
 
 public final class Balloon extends Aircraft {
-    Balloon(long p_id, String p_name, Coordinates p_coordinate) {
+    public Balloon(long p_id, String p_name, Coordinates p_coordinate) {
         super(p_id, p_name, p_coordinate);
     }
 
     @Override
     public void updateConditions() {
+        String weather = weatherTower.getWeather(coordinates);
+        int longitude = coordinates.getLongitude();
+        int latitude = coordinates.getLatitude();
+        int height = coordinates.getHeight();
+        switch (weather) {
+            case "SUN":
+                latitude += 2;
+                height += 4;
+                System.out.println("Balloon#" + name + "(" + id + "): Let's enjoy the good weather and take some pics.");
+                break;
+            case "RAIN":
+                height -= 5;
+                System.out.println("Balloon#" + name + "(" + id + "): Damn you rain! You messed up my balloon.");
+                break;
+            case "FOG":
+                height -= 3;
+                System.out.println("Balloon#" + name + "(" + id + "): Can't see anything through this fog.");
+                break;
+            case "SNOW":
+                height -= 15;
+                System.out.println("Balloon#" + name + "(" + id + "): It's snowing. We're gonna crash.");
+                break;
+            default:
+                // TODO: Throw a custom exception
+                throw new IllegalStateException("Unknown weather " + weather);
+        }
 
+        if (height <= 0) {
+            System.out.println("Balloon#" + name + "(" + id + "): Landing.");
+            weatherTower.unregister(this);
+            System.out.println("Tower says: Balloon#" + name + "(" + id + ") unregistered from weather tower.");
+        }
+        coordinates = new Coordinates(longitude, latitude, Math.min(height, 100));
     }
 }
